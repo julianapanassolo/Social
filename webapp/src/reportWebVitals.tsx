36c21200074695216250c03e4dbc559e5ed9aba3
juliana <juli.panassolo@gmail.com>
2025-05-22 15:55:57 -0300

    mais ajustes de js para tsx

--- webapp/src/reportWebVitals.tsx
+++ webapp/src/reportWebVitals.tsx
@@ -1,11 +1,10 @@
-const reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry: { (...data: any[]): void; (message?: any, ...optionalParams: any[]): void; }) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+    import('web-vitals').then((webVitals) => {
+      webVitals.onCLS(onPerfEntry);
+      webVitals.onFCP(onPerfEntry);
+      webVitals.onLCP(onPerfEntry);
+      webVitals.onTTFB(onPerfEntry);
     });
   }
 };
